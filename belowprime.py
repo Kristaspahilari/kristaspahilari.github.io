79fab8b63b200952d589d3f3eeb0ffd242ebecd9
Kristaspahilari <66671551+Kristaspahilari@users.noreply.github.com>
2021-07-07 13:04:18 +0200

    Add files via upload

--- belowprime.py
+++ belowprime.py
@@ -1,9 +1,11 @@
 def below_prime(n):
+    l = []
     for num in range(2, n):
-    
      if num > 1:
+    
        for i in range(2, num):
            if (num % i) == 0:
                break
        else:
-          print(list(num))
+          l.append(num)
+          print(l)
